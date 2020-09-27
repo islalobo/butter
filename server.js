30843d17ddefb7fbf8580714dc0a53ffaf4a42c9
alanna <alanna@sidedooraccess.com>
2020-09-26 23:47:50 -0300

    trying to get ffmpeg to work

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const ffmpeg = require('ffmpeg');
 const { promisify } = require('util');
 const { v4 } = require('uuid');
 
@@ -35,16 +36,44 @@ app.post('/messages', (req, res) => {
   }
   const messageId = v4();
   writeFile(messageFolder + messageId, req.body.message, 'base64')
-    .then(() => {
-      res.status(201).json({
-        message: 'Saved message',
-        id: messageId,
-      });
-    })
-    .catch(err => {
-      console.log('Error writing message to file', err);
-      res.sendStatus(500);
-    });
+  .then(() => {
+      console.log('messages was saved!');
+      try {
+        console.log('attempting to make an mp3 ', messageFolder + messageId);
+        const process = new ffmpeg(messageFolder + messageId);
+        process.then((audio) => {
+          console.log('there is audio, ', audio);
+          console.log('file is here ', messageFolder + messageId, `${messageFolder}${messageId}.mp3`);
+          audio.fnExtractSoundToMP3(`${messageFolder}${messageId}.mp3`, (error, file) => {
+            console.log('There is an error ', error);
+            console.log('There is a file ', file);
+            if (!error) {
+              console.log('Audio File ', file);
+              const file = file;
+
+              res.status(201).json({
+                message: 'Saved message',
+                id: messageId,
+              });
+            }
+          })
+        }, (err) => {
+          console.log('An error occurred, ', err);
+        });
+      } catch(e) {
+        console.log(e.code);
+        console.log(e.msg);
+      }
+
+    // res.status(201).json({
+    //   message: 'Saved message',
+    //   id: messageId,
+    // });
+  })
+  .catch(err => {
+    console.log('Error writing message to file', err);
+    res.sendStatus(500);
+  });
 });
 
 const PORT = process.env.PORT || 3545;
