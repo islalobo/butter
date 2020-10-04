d9e0fb3427c7ef3ede321c6348a8b184b635cdf2
alanna <alanna@sidedooraccess.com>
2020-10-04 17:18:59 -0300

    update routes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+// Begin
 const express = require('express');
 const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
 const { promisify } = require('util');
 const { v4 } = require('uuid');
+
+// Define app
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
 
 const writeFile = promisify(fs.writeFile);
 const readdir = promisify(fs.readdir);
@@ -13,17 +20,30 @@ if (!fs.existsSync(messageFolder)) {
   fs.mkdirSync(messageFolder);
 }
 
-const audioFolder = './public/messages/mp3/';
-if (!fs.existsSync(messageFolder)) {
-  fs.mkdirSync(messageFolder);
-}
+// Setup Pages
+var path = require('path');
+var htmlPath = path.join(__dirname, 'public');
 
-const app = express();
-app.use(express.json({limit: '50mb', extended: true}));
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static(htmlPath));
 
+// Pages
+// const animal = require('./public/');
+// const alive = require('./alive');
+// const black = require('./black');
+// const immigrant = require('./immigrant');
+// const queer = require('./queer');
+// const sister = require('./sister');
+
+// Routes
+// const routes = require('express').Router();
+// routes.use('/animal', animal);
+// routes.use('/alive', alive);
+// routes.use('/black', black);
+// routes.use('/immigrant', immigrant);
+// routes.use('/queer', queer);
+// routes.use('/sister', sister);
+
+// Methods
 app.get('/messages', (req, res) => {
   readdir(messageFolder)
     .then(messageFilenames => {
@@ -41,41 +61,23 @@ app.post('/messages', (req, res) => {
   }
   const messageId = v4();
   writeFile(messageFolder + messageId, req.body.message, 'base64')
-  .then(() => {
-      console.log('messages was saved!');
-      try {
-        console.log('attempting to make an mp3 ', `${messageFolder}${messageId}`, `${audioFolder}${messageId}.mp3`);
-        const process = new ffmpeg(`${messageFolder}${messageId}`);
-        process.then((audio) => {
-          console.log('audio is ready to be processed ', audio);
-          audio.fnExtractSoundToMP3(`${audioFolder}${messageId}.mp3`, (error, file) => {
-            console.log('There is an error ', error);
-            console.log('There is a file ', file);
-            if (!error) {
-              console.log('Audio File ', file);
-              const file = file;
-              console.log('what a success, ', messageId && messageId, file && file);
-              res.status(201).json({
-                message: 'Saved message',
-              });
-            }
-          })
-        }, (err) => {
-          console.log('An error occurred, ', err);
-        });
-      } catch(e) {
-        console.log('error code ', e.code);
-        console.log('error message ', e.msg);
-      }
+    .then(() => {
+      res.status(201).json({
+        message: 'Saved message',
+        id: messageId,
+      });
+    })
+    .catch(err => {
+      console.log('Error writing message to file', err);
+      res.sendStatus(500);
+    });
+});
 
-    // res.status(201).json({
-    //   message: 'Saved message',
-    //   id: messageId,
-    // });
-  })
-  .catch(err => {
-    console.log('Error writing message to file', err);
-    res.sendStatus(500);
+app.delete('/messages', (req, res) => {
+  // todo, delete message by id
+  res.status(201).json({
+    message: 'Nothing happened',
+    id: req.body.id,
   });
 });
 
